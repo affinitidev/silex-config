e6a0a6ee88ec80b2ac5974fdcb06364cb0d0a3ee
Brendan Bates <brendanbates89@gmail.com>
2014-06-01 09:19:29 -0400

    Fixes #3.

--- Source/Cache/CacheFactory.php
+++ Source/Cache/CacheFactory.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Affiniti Development
+ * 
  * Config Service Provider
  *
- * @link        http://github.com/affinitidev/SilexConfig
+ * @link        http://github.com/affinitidev/silex-config
  * @copyright   Brendan Bates (http://www.brendan-bates.com)
  * @license     http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
- * @version     0.1
  */
 
 namespace Affiniti\Config\Cache;
--- Tests/Fixtures/Config/not_array.php
+++ Tests/Fixtures/Config/not_array.php
@@ -1,3 +1,12 @@
 <?php
+/**
+ * Affiniti Development
+ * 
+ * Config Service Provider
+ *
+ * @link        http://github.com/affinitidev/silex-config
+ * @copyright   Brendan Bates (http://www.brendan-bates.com)
+ * @license     http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
+ */
 
 return 'not_an_array';
