bc24e331d911558f8dc6cb2863ff6e7aed31381a
Brendan Bates <brendanbates89@gmail.com>
2014-06-01 20:08:03 -0400

    Use new exception instead of Symfony exception for validation errors.

---
+++ Source/Exception/ValidationError.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Affiniti Development
+ * 
+ * Config Service Provider
+ *
+ * @link        http://github.com/affinitidev/silex-config
+ * @copyright   Brendan Bates (http://www.brendan-bates.com)
+ * @license     http://www.opensource.org/licenses/mit-license.php MIT (see the LICENSE file)
+ */
+
+namespace Affiniti\Config\Exception;
+
+/**
+ * Thrown if an error is encountered during validation.
+ * 
+ * @author Brendan Bates <me@brendan-bates.com>
+ */
+class ValidationError extends ConfigException { }
